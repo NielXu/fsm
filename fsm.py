6d998f32282bc259cc85a8cc743eea4f00eeaead
Daniel Xu <niellxu@yahoo.ca>
2019-04-10 14:47:32 -0400

    Added resources

--- fsm.py
+++ fsm.py
@@ -3,6 +3,9 @@ This module contains high level of finite state machine. It supports
 features such as storing values across each state and in the Machine.
 And also support boolean comparsion transit.
 """
+from simplefsm import RepeatedSignal, UnknownSignal
+
+
 class Signal():
     "A bool expression that being stored inside the signal"
     def __init__(self, bool_expr):
@@ -25,3 +28,42 @@ class Signal():
     
     def __repr__(self):
         return self.__str__()
+
+
+class State():
+    "A state representation on fsm"
+    def __init__(self, name, exe=lambda :None):
+        """
+        name: Name of this state\n
+        exe: Function that being called when it reaches this state, default is a
+            function that takes no argument and return None.
+        """
+        self.transit_map = {}
+        self.name = name
+        self.exe = exe
+    
+    def read(self, mapper):
+        """
+        Different from simple fsm, this state will NOT read a signal, it will use
+        the variable mapper and the internal signal to determine if transit to
+        next state or not. This state will transit to next state iff the signal
+        returns True
+        """
+
+    def add_transit(self, signal, next_state):
+        """
+        Add transition to next state. If state reads a signal, it
+        will go to a corresponding next state. Signals should be 
+        different, i.e one signal goes to one state. However, signal
+        can be defined as ANY, in this case, if the signal is unknown
+        for this state, it will automatically go to the state that ANY points to.
+        The ANY can be changed directly since it is a global variable.
+        Default is: ANY = "*"
+            add_transit(ANY, next_state)
+        """
+    
+    def execute(self):
+        """
+        Execute the function that this state contains. And return whatever the function returns
+        """
+        return self.exe()
